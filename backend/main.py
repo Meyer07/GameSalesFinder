280997542ae8c53bc1ba6314b228ae0dd342a2b0
Sam Meyer <smeyer1@new.rr.com>
2026-03-03 14:26:51 -0600

    fixed with import errors and venv chicanery on the backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastApi
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine
 import models
 from routes import users, wishlist
-from scheduler import startScheduler
+from scheduler import startScheduler 
+
