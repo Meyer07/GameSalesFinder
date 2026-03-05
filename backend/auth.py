491796d63bfe2ecc1f054785ed1a297b454853d7
Sam Meyer <smeyer1@new.rr.com>
2026-03-05 11:58:35 -0600

    backend files have been implemented onto the frontend

---
+++ backend/auth.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import get_db
+import models
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRETKEY=os.getenv("SECRETKEY","changethis")
+ALGORITHM=os.getenv("ALGORITHM","HS256")
+ACCESS_TOKEN_EXPIRE=os.getenv("TOKEN_EXPIRATION","10080")
+
+pwd_context   = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+
+def hash_password(password:str)->str:
+    return pwd_context.hash(password)
+
+def verify_password(plain:str,hashed:str)->str:
+    return pwd_context.verify(plain,hashed)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None)->str:
+    to_encode=data.copy()
+    expire=datetime.utcnow()+(expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE))
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode,SECRETKEY,ALGORITHM)
+
+
+def getCurrentUser(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRETKEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
+
